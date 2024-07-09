22453d20d0162de2dc267367e717671124a77998
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-07-10 00:35:42 +0700

    Add Wikimedia data producer module

---
+++ Wikimedia-data-producer/src/main/java/com/tienhuynh_tn/WikimediaDataProducerApplication.java
@@ -0,0 +1,11 @@
+package com.tienhuynh_tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaDataProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaDataProducerApplication.class, args);
+    }
+}
