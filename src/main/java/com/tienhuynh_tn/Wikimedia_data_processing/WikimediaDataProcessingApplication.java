c92397de8c94d9a44eb0b677cbd1d0618b8d4471
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-07-10 00:16:18 +0700

    Init project

---
+++ src/main/java/com/tienhuynh_tn/Wikimedia_data_processing/WikimediaDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.tienhuynh_tn.Wikimedia_data_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaDataProcessingApplication.class, args);
+	}
+
+}
